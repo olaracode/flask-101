203a45198d076f1a5ccb9674ee3d791992941191
Octavio Lara <octavioasapchi@gmail.com>
2024-07-01 23:18:18 -0300

    ADD ADD FULL CRUD

--- app.py
+++ app.py
@@ -1,85 +1,124 @@
-# Aqui vamos a importar flask
 from flask import Flask, jsonify, request
 
-# Esta linea instancia nuestra app de flask
+# Estatus 200, OK
+# Estatus 201, Created
+# Estatus 400, Bad Request
+# Estatus 401, Unauthorized -> No autorizado (Sesion)
+# Estatus 403, No Tienes permisos (Acceso/Rol)
+# Estatus 404, Not Found
+# Estatus 415, Content Type not Accepted
+# Estatus 500, Servidor
+
+# app -> es comun pero no obligacion
 app = Flask(__name__)
+
+# Encapsulacion. Todo lo relevante a manipular los datos de la clase
+# Debe vivir dentro de la clase
+
+# <Objt Estudiante#7411521>
 
 class Estudiante:
     def __init__(self, nombre, id):
         self.name = nombre
         self.id = id
 
+    # Metodos
+    # Agregan a nivel del init
+
+    def update(self, name):
+        self.name = name
+
     def serialize(self):
+        # Permitir que el objeto sea accesible
+        # Por cosas externas a la aplicaion
         return {
             "name": self.name,
             "id": self.id
         }
     
 estudiantes = []
-# URLs dentro de nuestra API que se encargan de distribuir informacion en concreto
-# Un endpoint con el metodo get
-# localhost:8000/
+
 @app.route("/", methods=["GET"])
 def get_app():
     return "Soy una API de flask"
 
-# Un decorador es una funcion que se ejecuta antes de otra funcion
-# localhost:8000/saluda
-@app.route("/saluda", methods=["GET"])
-def saluda(): # El nombre de la funcion me lo invento
-    return "Hola que tal?"
+# CRUD Create Read Update Delete
+# Usuario: Crear, Ver, Modificar, Borrar
+# Post: Crear, Ver, Modificar y Borrar
+# Comentario: Crear, ver, modificar y borrar
+# Estudiantes:
+# Metodo Get -> Ver todos los estudiantes
+# Metodo Post -> Para crear un nuevo estudiante
+# Modo Get -> Para ver un estudiante
 
 @app.route("/estudiantes", methods=["GET"])
 def get_estudiantes():
-    # Usando un bucle comun
-    # estudiantes_serializados = []
-    # for estudiante in estudiantes:
-    #     estudiantes_serializados.append(estudiante.serialize())
-
-    # Usando comprehensive list:
-    # estudiantes_serializados = [estudiante.serialize for estudiante in estudiantes]
-
-    # Usando el map()
     estudiantes_serializados = list(map(lambda estudiante: estudiante.serialize(), estudiantes))
     return jsonify({    
         "estudiantes": estudiantes_serializados,
         "cantidad": len(estudiantes)
     })
 
-@app.route("/estudiante", methods=["POST"])
-def crear_estudiante():
-    body = request.get_json() # {}
-    # Tenemos que recibir el nombre y el id
-    name = body.get("name", None) # diccionario["key"]
-    id = body.get("id", None) # No es OPCIONAl
-
-    if id is None:
-        return jsonify({"error": "El id es requerido"}), 400
-    if name is None:
-        return jsonify({"error": "El name es requerido"}), 400
-
-    # crear una nueva instancia de la clase
-    estudiante = Estudiante(name, id)
-
-    # agregarlo a la list estudiantes
-    estudiantes.append(estudiante)
-    # retornar correctamente la data <Object Estudiante at #1230981230192>
-    return jsonify(estudiante.serialize())
-
-# Parametro para hacer la ruta dinamica
-# @app.route("<tipo_dato:nombre_variable>")
-# def nombre_generico(nombre_variable)
+# Metodo methods=["GET"] -> GET
 @app.route("/estudiante/<int:id>")
 def get_estudiante_by_id(id):
     for estudiante in estudiantes:
         if estudiante.id == id:
             return jsonify(estudiante.serialize())
+        
+    # Retornamos un error 404 por defecto
     return jsonify({
         "error": "Estudiante no encontrado :("
     }), 404
 
+@app.route("/estudiante", methods=["POST"])
+def crear_estudiante():
+    body = request.get_json()
+    name = body.get("name", None) # body["name"]
+    id = body.get("id", None) 
 
-# Hot reload: Cuando haces un cambio se actualiza la app
-# Esto es lo que inicia la aplicacion como tal
+    if id is None:
+        # BE -> FE
+        return jsonify({"error": "El id es requerido"}), 400
+    if name is None:
+        return jsonify({"error": "El name es requerido"}), 400
+
+    # Creamos una instancia de la clase
+    estudiante = Estudiante(name, id)
+
+    # Agregamos un estudiante a la lista de estudiantes
+    estudiantes.append(estudiante)
+
+    # Es que cuando creemos siempre retornemos estatus 201
+    return jsonify(estudiante.serialize()), 201
+
+@app.route("/estudiante/<int:id>", methods=["DELETE"])
+def delete_estudent(id):
+    for estudiante in estudiantes:
+        if estudiante.id == id:
+            estudiantes.remove(estudiante)
+            return jsonify({"msg": "Estudiante eliminado"}), 200
+    # Significa que no lo encontro
+    return jsonify({"error": "Estudiante not found"}), 404
+
+@app.route("/estudiante/<int:id>", methods=["PUT"])
+def update_estudent(id):
+    body = request.get_json()
+    name = body.get("name", None)
+
+    if name is None or name == "":
+        return jsonify({"error": "El name es requerido"}), 400
+    
+    # Encontrar al estudiante que quiero actualizar
+    # ejecutar el metodo update de ese estudiante
+    # Y retornarlo
+
+    for estudiante in estudiantes:
+        if estudiante.id == id:
+            estudiante.update(name)
+            return jsonify(estudiante.serialize()), 200
+        
+    return jsonify({"error": "Estudiante no encontrado"}), 404
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
